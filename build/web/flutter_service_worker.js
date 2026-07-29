08ff2b8cc0666bc3d104cf540b2fa3c1ddd84abe
peterlinan-Gomez <peterlinan@gmail.com>
2026-07-29 00:00:58 -0500

    Forzar compilacion web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -289,7 +289,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3671fa7b0a9867ea796f0e9a5da08f16
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "cc6843fe4799ade16c8b039438ea0205",
+"flutter_bootstrap.js": "21a37facb1993d75ecb8992ed1e0ade2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
